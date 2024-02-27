cd68134f15ac7e09ccf993e332e994d29d5883e5
Mika Hänninen <mika.hanninen@gmail.com>
2024-02-27 18:13:06 +0200

    first commit

---
+++ src/mypackage/library.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class custom:
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def keyword_from_package(self):
+        message = "keyword from custom package"
+        self.logger.info(message)
+        return message
